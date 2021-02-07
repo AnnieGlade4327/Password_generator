7e7e7292ae248f3a86c27dda6b80aa5f48939520
Annie <58996371+AnnieGlade4327@users.noreply.github.com>
2021-02-06 18:30:43 -0700

    first edits to js file

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var specialChar = [];
+var numericChar =[];
+var lowerCase= [];
+var upperCase= [];
 
-  passwordText.value = password;
-
+function getOptions() {
+  var length= 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+if (isNaN(length)=== true) {
+    alert("password length needs a number");
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
